53c415fb4fe8c6fd61a98e1c127e2b516dc1ee7d
BADER M <badernezam@gmail.com>
2023-07-11 20:40:22 -0700

    Updated

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,48 +1,51 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
+
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: "development",
+    mode: 'development',
     entry: {
-      main: "./src/js/index.js",
-      install: "./src/js/install.js",
+      main: './src/js/index.js',
+      install: './src/js/install.js',
     },
     output: {
-      filename: "[name].bundle.js",
-      path: path.resolve(__dirname, "dist"),
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new NodePolyfillPlugin(),
       new HtmlWebpackPlugin({
-        template: "./index.html",
+        template: './index.html',
       }),
 
       new InjectManifest({
-        swSrc: "./src/js/sw.js",
-        swDest: "sw.js",
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
       }),
 
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "Just Another Text Editor",
-        short_name: "JATE",
-        description: "A simple text editor",
-        display: "standalone",
-        background_color: "#ffffff",
-        theme_color: "#000000",
-        start_url: "/",
-        publicPath: "/",
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'A simple text editor',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        start_url: '/',
+        publicPath: '/',
         icons: [
           {
-            src: path.resolve("src/assets/icons/icon-512x512.png"),
+            src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join("assets", "icons"),
+            destination: path.join('assets', 'icons'),
           },
         ],
       }),
@@ -52,24 +55,30 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
+          use: ['style-loader', 'css-loader'],
         },
         {
           test: /\.(png|jpe?g|gif|svg)$/i,
-          type: "asset/resource",
+          type: 'asset/resource',
         },
         {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
-              presets: ["@babel/preset-env"],
-              plugins: ["@babel/plugin-transform-runtime"],
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-transform-runtime'],
             },
           },
         },
       ],
     },
+    resolve: {
+      fallback: {
+        async_hooks: false,
+        fs: false
+      }
+    }
   };
 };
